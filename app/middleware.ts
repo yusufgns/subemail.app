b4bcd54ef5dc931f6664a387789e340c5e230c05
Yusuf Güneş <gunesyusuf838@gmail.com>
2023-08-31 14:20:41 +0300

    TEST

--- app/middleware.ts
+++ app/middleware.ts
@@ -3,18 +3,14 @@ import { NextResponse } from "next/server";
 export function middleware() {
     const res = NextResponse.next()
 
-    res.headers.append('Access-Control-Allow-Credentials', "true")
-    res.headers.append('Access-Control-Allow-Origin', '*')
-    res.headers.append('Access-Control-Allow-Methods', 'POST')
-    res.headers.append(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
+    res.headers.append('Access-Control-Allow-Origin', 'https://submails.vercel.app')
+
+    return res
 
     return res
 }
 
 // specify the path regex to apply the middleware to
 export const config = {
-    matcher: '/api/:path*',
+    matcher: '/pages/api/subscriber_email',
 }
