afdebfff8571446c28ccfddce4a64e303f898c3e
Yusuf Güneş <gunesyusuf838@gmail.com>
2023-08-31 14:22:36 +0300

    TEST

--- app/middleware.ts
+++ app/middleware.ts
@@ -3,9 +3,9 @@ import { NextResponse } from "next/server";
 export function middleware() {
     const res = NextResponse.next()
 
-    res.headers.append('Access-Control-Allow-Origin', 'https://submails.vercel.app')
-
-    return res
+    res.headers.append('Access-Control-Allow-Credentials', "true")
+    res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
 
     return res
 }
--- next.config.js
+++ next.config.js
@@ -7,9 +7,15 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/pages/api/subscriber_email',
+        // matching all API routes
+        source: '/api/:path*',
         headers: [
-          // Diğer başlıklar
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
         ],
       },
     ]
