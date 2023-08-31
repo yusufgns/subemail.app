7ff22b1f093b06931b6b6b081f88810411ae0193
Yusuf Güneş <gunesyusuf838@gmail.com>
2023-08-31 20:25:03 +0300

    TEST

--- next.config.js
+++ next.config.js
@@ -3,23 +3,6 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
-
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          // Diğer başlıklar
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,DELETE,PATCH,POST,PUT',
-          },
-        ],
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
