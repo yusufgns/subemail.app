b6e146680652da02173a19a07c8f5911ffe70e89
Yusuf Güneş <gunesyusuf838@gmail.com>
2023-09-02 00:30:22 +0300

    Test row controller

--- pages/api/v1/[emailController].ts
+++ pages/api/v1/[emailController].ts
@@ -17,7 +17,7 @@ export default async function handler(
   const { email, projectKey } = props
 
   if (!email || !projectKey) {
-    res.status(400).json({ error: 'Email and projectKey are required' })
+    res.status(400).json({ error: 'Email and emailKey are required' })
     return
   }
 
@@ -33,21 +33,30 @@ export default async function handler(
     return
   }
 
-  if (isHaveEmail?.length > 0) {
-    const data = isHaveEmail.find((item) => item.projectKey === projectKey)
-
-    if (data) {
-      return res
-        .status(400)
-        .json({ error: `Error : This email already exists in the whitelist` })
-    }
+  if (isHaveEmail?.length <= 0) {
+    await supabase.from('emailList').insert({
+      email: email,
+      projectKey: projectKey,
+      cretorEmailKey: emailController,
+    })
+    res.status(200).json({
+      error: `Success add email to whitelist`,
+    })
+  } else {
+    const data = isHaveEmail?.find((item) => item.projectKey === projectKey)
+    data
+      ? res.status(400).json({
+          error: `This email already exists in the whitelist`,
+        })
+      : await supabase.from('emailList').insert({
+          email: email,
+          projectKey: projectKey,
+          cretorEmailKey: emailController,
+        })
+        res.status(200).json({
+          error: `Success add email to whitelist`,
+        })
   }
 
-  await supabase.from('emailList').insert({
-    email: email,
-    projectKey: projectKey,
-    cretorEmailKey: emailController,
-  })
-
-  return res.status(200).json({ message: `Success : Email successfully added` })
+  return res.send(`Hey what are you doing here?`)
 }
