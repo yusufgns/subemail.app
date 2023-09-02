d6773ea21b22abf03204fb99a9c70c520fb22fa9
Yusuf Güneş <gunesyusuf838@gmail.com>
2023-09-02 20:31:39 +0300

    Test : Role and email list controller middleware

--- pages/api/v1/[emailController].ts
+++ pages/api/v1/[emailController].ts
@@ -1,14 +1,9 @@
-// pages/api/yourApiRoute.ts
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { rateLimitMiddleware } from '@/middleware/rateLimit'
 import supabase from '@/utils/supabase'
+import { NextApiRequest, NextApiResponse } from 'next'
 import NextCors from 'nextjs-cors'
-import { withRoleControl } from '@/middleware/roleControl/withRoleControl'
+import { rateLimitMiddleware } from '@/middleware/rateLimit'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
+const handle = async (req: NextApiRequest, res: NextApiResponse) => {
   await NextCors(req, res, {
     methods: ['POST'],
     origin: '*',
@@ -17,42 +12,40 @@ export default async function handler(
 
   await rateLimitMiddleware(req, res)
 
-  const props = JSON.parse(req.body)
-  const { email, projectKey } = props
+  const data = JSON.parse(req.body)
   const { emailController } = req.query
+  const { email, projectKey } = data
 
   if (!email || !projectKey) {
-    res.status(400).json({ error: 'Email and projectKey are required' })
+    res.status(400).json({ error: 'Email and Project Key are required' })
     return
   }
 
-  const {
-    data: existingRowData,
-    error: existingRowError,
-  } = await supabase.from('emailList').select('*').eq('email', email)
+  const { data: isHaveEmail, error: isHaventEmail } = await supabase
+    .from('emailList')
+    .select('*')
+    .eq('email', email)
 
-  if (existingRowError) {
-    res.status(400).json({
-      error: `Something went wrong while connecting to Supabase | Error 400`,
+  const isEmailData = isHaveEmail?.length ?? 0
+
+  if (isEmailData <= 0) {
+    await supabase.from('emailList').insert({
+      email: email,
+      projectKey: projectKey,
+      emailController: emailController,
     })
-    return false
+  } else {
+    const data = isHaveEmail?.find((item) => item.projectKey === projectKey)
+    data
+      ? ''
+      : await supabase.from('emailList').insert({
+          email: email,
+          projectKey: projectKey,
+          emailController: emailController,
+        })
   }
 
-  const checkData = existingRowData.length ?? 0
-
-  if(!checkData) {
-    await withRoleControl(req)
-  }
-
-  if (checkData) {
-    const data = existingRowData.find((item) => item.projectKey === projectKey)
-    if (data) {
-      await supabase.from('emailList').insert({
-        email: 'gunesyusuf024@gmail.com',
-        projectKey: 'flksdfjnsdfjn',
-      })
-    }
-  }
-
-  return res.status(200).json({ message: `Success: Email successfully added` })
+  res.send('Success your request!')
 }
+
+export default handle
