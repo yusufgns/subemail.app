ba2b1835c46a28d97feb25a84228e40a79210a1d
Yusuf Güneş <gunesyusuf838@gmail.com>
2023-09-02 19:59:22 +0300

    Test : Role and email list controller middleware

--- middleware/roleControl/withRoleControl.ts
+++ middleware/roleControl/withRoleControl.ts
@@ -8,6 +8,7 @@ import { withhighEmailList } from '@/middleware/emailList/highEmailList'
 
 export async function withRoleControl(req: NextApiRequest) {
   const { emailController } = req.query
+  const { email, projectKey } = req.body
   const res = NextResponse.next() as any
 
   const { data: isHereData, error } = await supabase
@@ -18,19 +19,82 @@ export async function withRoleControl(req: NextApiRequest) {
   isHereData?.map(async (item) => {
     switch (item.role) {
       case 'free':
-        withFreeEmailList(req)
+        const { data: isFreeData, error: isFreeError } = await supabase
+          .from('emailList')
+          .insert([
+            {
+              email: 'free@gmail.com',
+              projectKey: projectKey,
+              cretorEmailKey: emailController,
+            },
+          ])
+
+        if (isFreeData) {
+          res.status(200).json({ message: `Success: Email successfully added` })
+        } else {
+          res
+            .status(400)
+            .json({ error: `Failed to add the row to the database` })
+        }
         break
 
       case 'low':
-        withLowEmailList(req)
+        const { data: isLowData, error: isLowError } = await supabase
+          .from('emailList')
+          .insert([
+            {
+              email: 'low@gmail.com',
+              projectKey: projectKey,
+              cretorEmailKey: emailController,
+            },
+          ])
+
+        if (isLowData) {
+          res.status(200).json({ message: `Success: Email successfully added` })
+        } else {
+          res
+            .status(400)
+            .json({ error: `Failed to add the row to the database` })
+        }
         break
 
       case 'middle':
-        withMiddleEmailList(req)
+        const {
+          data: isMiddleData,
+          error: isMiddleError,
+        } = await supabase.from('emailList').insert([
+          {
+            email: 'middle@gmail.com',
+            projectKey: projectKey,
+            cretorEmailKey: emailController,
+          },
+        ])
+        if (isMiddleData) {
+          res.status(200).json({ message: `Success: Email successfully added` })
+        } else {
+          res
+            .status(400)
+            .json({ error: `Failed to add the row to the database` })
+        }
         break
 
       case 'high':
-        withhighEmailList(req)
+        const { data: isHighData, error: isHighError } = await supabase
+          .from('emailList')
+          .insert([
+            {
+              email: 'high@gmail.com',
+              projectKey: projectKey,
+              cretorEmailKey: emailController,
+            },
+          ])
+        if (isHighData) {
+          res.status(200).json({ message: `Success: Email successfully added` })
+        } else {
+          res
+            .status(400)
+            .json({ error: `Failed to add the row to the database` })
+        }
         break
       default:
         NextResponse.next()
