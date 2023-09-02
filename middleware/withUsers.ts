f70d877642429af4f346adb1fa40c8adefe5e920
Yusuf Güneş <gunesyusuf838@gmail.com>
2023-09-02 13:02:58 +0300

    Test : product type

--- middleware/withUsers.ts
+++ middleware/withUsers.ts
@@ -1,18 +1,31 @@
-import { NextFetchEvent, NextMiddleware, NextRequest, NextResponse } from 'next/server'
+import {
+  NextFetchEvent,
+  NextMiddleware,
+  NextRequest,
+  NextResponse,
+} from 'next/server'
 import { MiddlewareFactory } from './types'
 import supabase from '@/utils/supabase'
 
 export const withUsers: MiddlewareFactory = (next: NextMiddleware) => {
-  return async (request: NextRequest, _next: NextFetchEvent, response: NextResponse) => {
-    const { data: emailList, error } = await supabase.from('emailList').insert([{email: 'testers@gmail.com', projectKey: 'testersMan', cretorEmailKey: 'testerMan'}])
-    
+  return async (request: NextRequest, _next: NextFetchEvent) => {
+    const { data: emailList, error } = await supabase
+      .from('emailList')
+      .insert([
+        {
+          email: 'testers@gmail.com',
+          projectKey: 'testersMan',
+          cretorEmailKey: 'testerMan',
+        },
+      ])
+
     console.log(emailList)
 
     if (error) {
       console.error('Error fetching user data:', error)
-      return response.status(500).json({ error: 'Internal Server Error' })
+      return console.error(error)
     }
 
-    return next(request, _next, response)
+    return next(request, _next)
   }
 }
