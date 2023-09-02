a54a38a9911c074639dd912435372bf12e9e9176
Yusuf Güneş <gunesyusuf838@gmail.com>
2023-09-02 20:49:28 +0300

    Fix

--- pages/api/v1/[emailController].ts
+++ pages/api/v1/[emailController].ts
@@ -10,46 +10,13 @@ const handle = async (req: NextApiRequest, res: NextApiResponse) => {
     optionsSuccessStatus: 200,
   })
 
-  await rateLimitMiddleware(req, res)
+  await supabase.from('emailList').insert([{
+    email: "req.body.email",
+    projectKey: "req.body.projectKe",
+    emailController: "req.body.emailController",
+  }])
 
-  const data = JSON.parse(req.body)
-  const { emailController } = req.query
-  const { email, projectKey } = data
-
-  if (!email || !projectKey) {
-    res.status(400).json({ error: 'Email and Project Key are required' })
-    return
-  }
-
-  const { data: isHaveEmail, error: isHaventEmail } = await supabase
-    .from('emailList')
-    .insert([{ email: "tester", projectKey: "tester", emailController: "tester" }])
-
-  /* const { data: isHaveEmail, error: isHaventEmail } = await supabase
-    .from('emailList')
-    .select('*')
-    .eq('email', email)
-
-  const isEmailData = isHaveEmail?.length ?? 0
-
-  if (isEmailData <= 0) {
-    await supabase.from('emailList').insert({
-      email: email,
-      projectKey: projectKey,
-      emailController: emailController,
-    })
-  } else {
-    const data = isHaveEmail?.find((item) => item.projectKey === projectKey)
-    data
-      ? ''
-      : await supabase.from('emailList').insert({
-          email: email,
-          projectKey: projectKey,
-          emailController: emailController,
-        })
-  } */
-
-  res.send('Success your request!')
+  return res.send('Success your request!')
 }
 
 export default handle
