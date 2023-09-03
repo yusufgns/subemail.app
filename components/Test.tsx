89481072bbba6f9dc5a615a4d5c0f64a1ddec758
Yusuf Güneş <gunesyusuf838@gmail.com>
2023-09-03 10:42:12 +0300

    Fix : Vercel bug

--- components/Test.tsx
+++ components/Test.tsx
@@ -17,7 +17,7 @@ export default function Test() {
   const emailss = 'TESTTTASDAD'
 
   const onSubmit = async () => {
-    const data = await fetch('https://submails.vercel.app/api/v1/9199f124-e1bb-4413-aec0-f508a3c5516d', {
+    const data = await fetch('https://submails.vercel.app/api/v1', {
       body: JSON.stringify({
         email: "gunesyusuf024@gmailasdasdasdas000000000000dadsasda.com",
         projectKey: "631c3e83-0c33-4f72-ac00-9eef0aafae8a"
